9fdfa80953fa14f62d6dd58aef8e6e1d69e77495
notansjwmember <pw3rapid.sniper@gmail.com>
2025-06-28 23:41:05 +0800

    feat: constant for positions, dashboard config

---
+++ src/app/dashboard/_config/permissions.ts
@@ -0,0 +1,7 @@
+// example, for future use case
+//
+export const PERMISSIONS = {
+  "view-users": ["admin"],
+  "edit-profile": ["admin", "officer", "student"],
+  "approve-request": ["admin", "officer"],
+};
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,26 @@
-import { NavItem, Role } from "./types";
+export const Semesters = [1, 2] as const;
+
+export const YearLevels = [1, 2, 3, 4] as const;
+
+export const Courses = ["BSIT", "BSCS"] as const;
 
 export const Roles = ["admin", "officer", "student"] as const;
-export const YearLevels = [1, 2, 3, 4] as const;
-export const Courses = ["BSIT", "BSCS"] as const;
+
+export const Positions = {
+  PRESIDENT: "President",
+  IVP: "Internal Vice President",
+  EVP: "External Vice President",
+  SEC_GEN: "Secretary General",
+  SEC_ASS: "Assistant Secretary",
+  TREASURER: "Treasurer",
+  TREASURER_ASS: "Assistant Treasurer",
+  AUDITOR: "Auditor",
+  BS_MAN: "Business Manager",
+  PRO: "Public Relations Officer",
+  PIO: "Public Information Officer",
+  IT_REP: "IT Representative",
+  CS_REP: "CS Representative",
+} as const;
 
 export enum IconSizes {
   XL = 24,
@@ -11,9 +29,3 @@ export enum IconSizes {
   SMALL = 18,
   XS = 16,
 }
-
-export const NAV_ITEMS: Record<Role, NavItem[]> = {
-  admin: [],
-  officer: [],
-  student: [],
-};
